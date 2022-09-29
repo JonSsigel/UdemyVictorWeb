3175cb5aa716d697650009528e840d6810f1ae80
JonS Gtz <jonsgutierrezp@gmail.com>
2022-09-28 20:32:56 -0500

    Final de Clase 22

---
+++ JS/hola-mundo/js/05-tipos-de-operadores.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// operadores y tipos de datos
+// Operadores
+
+var numero = 7;
+
+var numero2 = 12;
+
+var operacion = numero - numero2; // + . * / %
+
+alert("Resultado de operacion: " + operacion); 
+
+// Tipos de datos
+
+var numero_entero = 44;
+var cadena_texto = "Hola que tal"; // las comillas dobles tienen prioridad ante las comillas simples.
+
+var verdadero_o_falso = false; // o true
+
+
+var numero_falso = "33";
+// Funcion Number(), number hace un número cualquiera. parseInt() parseFloat()
+
+alert(numero_falso + 7);
+
+alert(Number(numero_falso)+7);
+
+console.log(String(4)+7);
